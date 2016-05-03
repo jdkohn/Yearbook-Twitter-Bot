794cfd321c910b4ed02aff4ec2cd142a1e7ab197
jdkohn <jdkohn18@gmail.com>
2016-05-03 11:55:23 -0700

    Searching Works, Starting to Run

---
+++ ybkbottests.php
@@ -0,0 +1,102 @@
+<?php
+
+include "createconnection.php";
+
+require_once('TwitterAPIExchange.php');
+
+
+$username = $argv[1];
+
+echo $username;
+
+// $settings = array(
+// 	'oauth_access_token' => "441378114-UH2UYwqhnQ9av7sWpJgZBHdIxxbNOEegYQqBlMTG",
+// 	'oauth_access_token_secret' => "PdMjEa9xT0hrNVhD9ogGq1bKOcMtthmRABStPx4hXWvhL",
+// 	'consumer_key' => "vWQ7bggg67cAKtuHF2m6YKLNB",
+// 	'consumer_secret' => "MBXAFbar7AYIfjYMURMjM6FLgQczpzxpWrhdxK490RlMFCndfB"
+// 	);
+
+
+
+// /** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
+// $url = 'https://api.twitter.com/1.1/followers/list.json';
+// $requestMethod = 'GET';
+// $getfield = '?screen_name=@' . $username . "&count=200";
+
+// // 		// Perform the request
+// $twitter = new TwitterAPIExchange($settings);
+// $json = $twitter->setGetfield($getfield)
+// ->buildOauth($url, $requestMethod)
+// ->performRequest();
+
+
+// $jsonObject = json_decode($json, true);
+
+// $json_string = json_encode($jsonObject, JSON_PRETTY_PRINT);
+
+// echo $json_string;
+
+// $nextCursor = $jsonObject["next_cursor_str"];
+
+// $counter = 0;
+
+// for($i=0; $i<count($jsonObject["users"]); $i++) {
+
+// 	$name=$jsonObject["users"][$i]["name"];
+// 	$screenname=$jsonObject["users"][$i]["screen_name"];
+
+
+// 	$firstlast = explode(" ", $name);
+// 	$last = $firstlast[count($firstlast) - 1];
+// 	$first = $firstlast["0"];
+
+// 	foreach($conn->query("SELECT * FROM students WHERE last='$last' AND username IS NULL") as $student) {
+// 		$studentname = $student["name"];
+// 		if(strpos(strtolower($screenname), strtolower($first)) == TRUE) {
+// 			$conn->query("UPDATE students SET username='$screenname' WHERE name='$studentname'");
+// 		} else if(strpos(strtolower($name), strtolower($first)) == TRUE) {
+// 			$conn->query("UPDATE students SET username='$screenname' WHERE name='$studentname'");
+// 		}
+// 	}
+// 	$counter++;
+// }
+
+
+// if($nextCursor !== "0") {
+// 	while($nextCursor != "0") {
+
+// 		echo "NEW CURSOR" . PHP_EOL;
+
+// 		$url = 'https://api.twitter.com/1.1/followers/list.json';
+// 		$requestMethod = 'GET';
+// 		$getfield = '?screen_name=@' . $username . "&count=200&cursor=" . $nextCursor;
+
+// 		// 		// Perform the request
+// 		$twitter = new TwitterAPIExchange($settings);
+// 		$json = $twitter->setGetfield($getfield)
+// 		->buildOauth($url, $requestMethod)
+// 		->performRequest();
+
+// 		$jsonObject = json_decode($json, true);
+
+		
+
+// 		for($i=0; $i<count($jsonObject["users"]); $i++) {
+
+// 			$counter++;
+
+// 			// $name=$jsonObject["users"][$i]["name"];
+// 			// $screenname=$jsonObject["users"][$i]["screen_name"];
+			
+
+// 			// if(mysqli_num_rows($conn->query("SELECT * FROM students WHERE name='$name' AND username IS NULL")) > 0) {
+// 			// 	$conn->query("UPDATE students SET username='$screenname' WHERE name='$name'");
+// 			// }
+// 		}
+// 		$nextCursor = $jsonObject["next_cursor_str"];
+// 	} 
+// }
+
+// echo $counter;
+
+?>
--- yearbookbot.php
+++ yearbookbot.php
@@ -4,7 +4,7 @@ include "createconnection.php";
 
 require_once('TwitterAPIExchange.php');
 
-$username = $argv[1];
+//$username = $argv[1];
 
 $settings = array(
 	'oauth_access_token' => "441378114-UH2UYwqhnQ9av7sWpJgZBHdIxxbNOEegYQqBlMTG",
@@ -14,43 +14,132 @@ $settings = array(
 	);
 
 
+foreach($conn->query("SELECT * FROM students WHERE username IS NOT NULL AND checked=0") as $student) {
+
+	$username = $student["username"];
+
 	/** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
 	$url = 'https://api.twitter.com/1.1/followers/list.json';
 	$requestMethod = 'GET';
 	$getfield = '?screen_name=@' . $username . "&count=200";
 
-	// Perform the request
+		// Perform the request
 	$twitter = new TwitterAPIExchange($settings);
 	$json = $twitter->setGetfield($getfield)
 	->buildOauth($url, $requestMethod)
 	->performRequest();
 
+	echo "REQUEST MADE: " . $username . PHP_EOL;
 
 	$jsonObject = json_decode($json, true);
 
-	$json_string = json_encode($jsonObject, JSON_PRETTY_PRINT);
-
-	//echo $json_string . PHP_EOL;
-
-	echo "COUNT: " . count($jsonObject["users"]) . PHP_EOL;
-
-	for($i=0; $i<count($jsonObject["users"]); $i++) {
-
-		//check to see if is a student -> name matches, last name matches, something along 
-		//							   ->those lines, check with Mr. Snyder to see how
-		//							   ->how accurate we need to be
-
-		//check to see if in mysql database
-
-		//
-
-		//add into mysql database
-		//$conn->query("UPDATE halestudents SET username='$screenname' WHERE name='$name'")
-
-		//add into array
-
-		//go to next "counter" if necessary
-
-		//move to next user
+	if(isset($jsonObject["errors"])) {
+		if($jsonObject["errors"][0]["message"] == "Rate limit exceeded") {
+			echo "LIMIT EXCEEDED" . PHP_EOL;
+			break;
+		}
 	}
+
+	if(isset($jsonObject["next_cursor_str"])) {
+		$nextCursor = $jsonObject["next_cursor_str"];
+	} else {
+		$nextCursor = 0;
+	}
+
+	//echo "NEXT CURSOR: " . $nextCursor . PHP_EOL;
+
+	if(isset($jsonObject["users"])) {
+		for($i=0; $i<count($jsonObject["users"]); $i++) {
+
+			$name=$jsonObject["users"][$i]["name"];
+
+			$screenname = $jsonObject["users"][$i]["screen_name"];
+
+			$q = "SELECT * FROM students WHERE name='$name' AND username IS NULL";
+
+			foreach($conn->query($q) as $stu) {
+				$conn->query("UPDATE students SET username='$screenname' WHERE name='$name'");
+
+				echo "ADDED USERNAME FOR: " . $name . PHP_EOL;
+			}
+
+			$firstlast = explode(" ", $name);
+			$last = $firstlast[count($firstlast) - 1];
+			$first = $firstlast["0"];
+
+			foreach($conn->query("SELECT * FROM students WHERE last='$last' AND username IS NULL") as $student) {
+				$studentname = $student["name"];
+				if(strpos(strtolower($screenname), strtolower($first)) == TRUE) {
+					$conn->query("UPDATE students SET username='$screenname' WHERE name='$studentname'");
+					echo "*ADDED USERNAME FOR: " . $name . PHP_EOL;
+				} else if(strpos(strtolower($name), strtolower($first)) == TRUE) {
+					$conn->query("UPDATE students SET username='$screenname' WHERE name='$studentname'");
+					echo "*ADDED USERNAME FOR: " . $name . PHP_EOL;
+				}
+			}
+		}
+	}
+
+	while($nextCursor != "0") {
+		$url = 'https://api.twitter.com/1.1/followers/list.json';
+		$requestMethod = 'GET';
+		$getfield = '?screen_name=@' . $username . "&count=200&cursor=" . $nextCursor;
+
+		// 		// Perform the request
+		$twitter = new TwitterAPIExchange($settings);
+		$json = $twitter->setGetfield($getfield)
+		->buildOauth($url, $requestMethod)
+		->performRequest();
+
+		$jsonObject = json_decode($json, true);
+
+		if(isset($jsonObject["errors"])) {
+			if($jsonObject["errors"][0]["message"] == "Rate limit exceeded") {
+				echo "LIMIT EXCEEDED" . PHP_EOL;
+				break;
+			}
+		}
+
+
+		if(isset($jsonObject["next_cursor_str"])) {
+			$nextCursor = $jsonObject["next_cursor_str"];
+		} else {
+			$nextCursor = 0;
+		}
+
+		//echo "NEXT CURSOR: " . $nextCursor . PHP_EOL;
+
+		if(isset($jsonObject["users"])) {
+			for($i=0; $i<count($jsonObject["users"]); $i++) {
+
+				$name=$jsonObject["users"][$i]["name"];
+				$screenname = $jsonObject["users"][$i]["screen_name"];
+
+				$q = "SELECT * FROM students WHERE name='$name' AND username IS NULL";
+
+				foreach($conn->query($q) as $stu) {
+					$conn->query("UPDATE students SET username='$screenname' WHERE name='$name'");
+					echo "ADDED USERNAME FOR: " . $name . PHP_EOL;
+				}
+			}
+
+			$firstlast = explode(" ", $name);
+
+			$last = $firstlast[count($firstlast) - 1];
+			$first = $firstlast[0];
+
+			foreach($conn->query("SELECT * FROM students WHERE last='$last' AND username IS NULL") as $student) {
+				$studentname = $student["name"];
+				if(strpos(strtolower($screenname), strtolower($first)) == TRUE) {
+					$conn->query("UPDATE students SET username='$screenname' WHERE name='$studentname'");
+					echo "*ADDED USERNAME FOR: " . $name . PHP_EOL;
+				} else if(strpos(strtolower($name), strtolower($first)) == TRUE) {
+					$conn->query("UPDATE students SET username='$screenname' WHERE name='$studentname'");
+					echo "*ADDED USERNAME FOR: " . $name . PHP_EOL;
+				}
+			}
+		}
+	}
+	$conn->query("UPDATE students SET checked=1 WHERE username='$username'");
+}
 ?>
